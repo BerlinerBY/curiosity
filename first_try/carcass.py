4d1f467bf4f26989128f5cc6645234d071d871c8
BerlinerBY <71092073+BerlinerBY@users.noreply.github.com>
2021-08-11 16:34:54 +0300

    Add widgets in hide page

--- first_try/carcass.py
+++ first_try/carcass.py
@@ -15,16 +15,16 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
-        MainWindow.setMaximumSize(QtCore.QSize(1000, 1000))
         MainWindow.setStyleSheet("border: None;")
-        MainWindow.setWindowIcon(QtGui.QIcon('wheel.png'))
         MainWindow.setWindowFlag(QtCore.Qt.FramelessWindowHint)
 
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.centralwidget.setStyleSheet("background-color: rgb(55, 59, 89);")
 
-        # add hint-panel
+        """
+        add hint-panel
+        """
         self.hint = QtWidgets.QFrame(self.centralwidget)
         self.hint.setGeometry(0, 0, 802, 30)
         self.hint.setStyleSheet("background: rgb(50, 50, 70);")
@@ -60,12 +60,14 @@ class Ui_MainWindow(object):
                                            )
         self.minimize_button.setObjectName("minimize")
 
-        # create areas with page
+        """
+        create areas with page
+        """
         self.windows = QtWidgets.QTabWidget(self.centralwidget)
         self.windows.setEnabled(True)
         self.windows.setGeometry(
             QtCore.QRect(0, 30, 802, 602))  # i use 802 and 602 px, because i don`t know, to fix bug
-        # with white border of pages
+                                            # with white border of pages
         self.windows.setStyleSheet("QTabBar::tab { "
                                    "    width: 50px; "
                                    "    height: 50px; "
@@ -86,7 +88,9 @@ class Ui_MainWindow(object):
         self.windows.setTabBarAutoHide(False)  # i can delete this string, but i don`t use this function
         self.windows.setObjectName("windows")
 
-        # start page
+        """
+        start page
+        """
         self.home = QtWidgets.QWidget()
         self.home.setStyleSheet("QWidget {"
                                 "    background-image: url('style/space.jpg');"
@@ -130,7 +134,9 @@ class Ui_MainWindow(object):
                                    "}"
                                    )
 
-        # page where encrypt-algorithm start
+        """
+        page where hide-algorithm start
+        """
         self.input = QtWidgets.QWidget()
         self.input.setStyleSheet("QWidget {\n"
                                  "    background-color: rgb(177, 40, 255);\n"
@@ -138,18 +144,105 @@ class Ui_MainWindow(object):
         self.input.setObjectName("input")
         self.windows.addTab(self.input, "")
 
-        # page where decryption-algorithm start
+        self.input_adress_file = QtWidgets.QLineEdit(self.input)
+        self.input_adress_file.setGeometry(30, 30, 270, 25)
+        self.input_adress_file.setPlaceholderText(" Please enter container address...")
+        self.input_adress_file.setStyleSheet("QLineEdit {"
+                                             "    background-color: rgb(255, 170, 0);"
+                                             "    border: none;"
+                                             "}")
+        self.input_adress_file.setObjectName("input_adress_file")
+
+        self.clear_line_adress = QtWidgets.QPushButton(self.input)
+        self.clear_line_adress.setGeometry(QtCore.QRect(300, 30, 50, 25))
+        self.clear_line_adress.setStyleSheet("QPushButton {"
+                                             "  background-color: rgb(80, 100, 127);"
+                                             "  border: 2px solid rgb(250, 250, 250);"
+                                             "}"
+                                             "QPushButton:hover:!pressed {"
+                                             "  border: 2px solid rgb(100, 100, 100);"
+                                             "}"
+                                             "QPushButton:pressed {"
+                                             "  background: rgb(70, 100, 117);"
+                                             "}")
+        self.clear_line_adress.setObjectName("clear")
+
+        self.open_file = QtWidgets.QPushButton(self.input)
+        self.open_file.setGeometry(QtCore.QRect(350, 30, 75, 25))
+        self.open_file.setStyleSheet("background-color: rgb(80, 100, 127);")
+        self.open_file.setObjectName("open_file")
+
+        self.input_save_adress_file = QtWidgets.QLineEdit(self.input)
+        self.input_save_adress_file.setGeometry(QtCore.QRect(30, 85, 270, 25))
+        self.input_save_adress_file.setPlaceholderText(" Please enter a save address...")
+        self.input_save_adress_file.setStyleSheet("QLineEdit {\n"
+                                                  "    background-color: rgb(255, 170, 0);\n"
+                                                  "    border: None;\n"
+                                                  "}")
+        self.input_save_adress_file.setObjectName("input_save_adress_file")
+
+        self.clear_save_line_adress = QtWidgets.QPushButton(self.input)
+        self.clear_save_line_adress.setGeometry(QtCore.QRect(300, 85, 50, 25))
+        self.clear_save_line_adress.setStyleSheet("background-color: rgb(80, 100, 127);")
+        self.clear_save_line_adress.setObjectName("clear")
+
+        self.save_file = QtWidgets.QPushButton(self.input)
+        self.save_file.setGeometry(QtCore.QRect(350, 85, 75, 25))
+        self.save_file.setStyleSheet("background-color: rgb(80, 100, 127);")
+        self.save_file.setObjectName("save_file")
+
+        self.input_text = QtWidgets.QPlainTextEdit(self.input)
+        self.input_text.setPlaceholderText("Please enter text...")
+        self.input_text.setGeometry(QtCore.QRect(450, 30, 270, 230))
+        self.input_text.setStyleSheet("QPlainTextEdit {"
+                                      "    background-color: rgb(255, 170, 0);"
+                                      "    border: None;"
+                                      "}")
+        self.input_text.setObjectName("input_text")
+
+        self.clear_input_text = QtWidgets.QPushButton(self.input)
+        self.clear_input_text.setGeometry(QtCore.QRect(450, 285, 125, 25))
+        self.clear_input_text.setStyleSheet("background-color: rgb(80, 100, 127);")
+        self.clear_input_text.setObjectName("clear_button")
+
+        self.hide_button = QtWidgets.QPushButton(self.input)
+        self.hide_button.setGeometry(QtCore.QRect(300, 140, 125, 25))
+        self.hide_button.setStyleSheet("background-color: rgb(0, 170, 127);")
+        self.hide_button.setObjectName("hide")
+
+        self.hide_progress = QtWidgets.QProgressBar(self.input)
+        self.hide_progress.setGeometry(30, 200, 390, 30)
+        self.hide_progress.setStyleSheet("background: rgb(28, 181,10);")
+        self.hide_progress.setObjectName("hide_progress")
+
+        self.output_recovery_key = QtWidgets.QTextEdit(self.input)
+        self.output_recovery_key.setGeometry(QtCore.QRect(30, 350, 390, 30))
+        self.output_recovery_key.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.output_recovery_key.setObjectName("output_key")
+        self.output_recovery_key.setPlaceholderText("Your recovery key...")
+
+        self.output_RSA_key = QtWidgets.QTextEdit(self.input)
+        self.output_RSA_key.setGeometry(QtCore.QRect(30, 410, 390, 30))
+        self.output_RSA_key.setStyleSheet("background-color: rgb(255, 255, 255);")
+        self.output_RSA_key.setObjectName("output_key")
+        self.output_RSA_key.setPlaceholderText("Your RSA key...")
+
+        """
+        page where recovery-algorithm start
+        """
         self.output = QtWidgets.QWidget()
-        self.output.setStyleSheet("QWidget {\n"
-                                  "    background-color: rgb(77, 40, 255);\n"
+        self.output.setStyleSheet("QWidget {"
+                                  "    background-color: rgb(77, 40, 255);"
                                   "}")
         self.output.setObjectName("output")
         self.windows.addTab(self.output, "")
 
-        # page with help-info
+        """
+        page with help-info
+        """
         self.support = QtWidgets.QWidget()
-        self.support.setStyleSheet("QWidget {\n"
-                                   "    background-color: rgb(77, 140, 255);\n"
+        self.support.setStyleSheet("QWidget {"
+                                   "    background-color: rgb(77, 140, 255);"
                                    "}")
         self.support.setObjectName("support")
         self.windows.addTab(self.support, "")
@@ -162,20 +255,19 @@ class Ui_MainWindow(object):
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
-        self.windows.setCurrentIndex(0)
+        self.windows.setCurrentIndex(1)  # не забыть поставить в конце работы 0
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
 
-        ''' i don`t use names of button
-        self.windows.setTabText(self.windows.indexOf(self.home), _translate("MainWindow", "Home"))
-        self.windows.setTabText(self.windows.indexOf(self.input), _translate("MainWindow", "Input"))
-        self.windows.setTabText(self.windows.indexOf(self.output), _translate("MainWindow", "Output"))
-        self.windows.setTabText(self.windows.indexOf(self.support), _translate("MainWindow", "Support"))
-        '''
-
+        self.clear_line_adress.setText(_translate("MainWindow", "Clear"))
+        self.open_file.setText(_translate("MainWindow", "Chek file"))
+        self.clear_save_line_adress.setText(_translate("MainWindow", "Clear"))
+        self.save_file.setText(_translate("MainWindow", "Chek save"))
+        self.hide_button.setText(_translate("MainWindow", "Hide"))
+        self.clear_input_text.setText(_translate("MainWindow", "Clear"))
 
 if __name__ == "__main__":
     import sys
